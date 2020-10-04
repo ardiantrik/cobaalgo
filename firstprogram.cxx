d21ec6cc642ac819c2cd5078e2323b903aef3592
Ardian Tri Kusuma <45714833+ardiantrik@users.noreply.github.com>
2020-10-04 22:23:44 +0700

    update file firstprogram

--- firstprogram.cxx
+++ firstprogram.cxx
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-	cout<<"Nama : siapa saya?"<<endl;
-	cout<<"NIM  : 123000000"<<endl;
+	cout<<"Nama : siapa namaku?"<<endl;
+	cout<<"NIM  : 123200001"<<endl;
 	cout<<"Dan seterusnya ...";
 	return 0;
 }
