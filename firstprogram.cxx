a9fbe2d13477fcdb058e6387517a790053740fdf
ardiantrik <ardiantrik@gmail.com>
2020-10-04 22:28:14 +0700

    ubah nim sesuka aku hehe

--- firstprogram.cxx
+++ firstprogram.cxx
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	cout<<"Nama : siapa namaku?"<<endl;
-	cout<<"NIM  : 123200001"<<endl;
+	cout<<"NIM  : 123200100"<<endl;
 	cout<<"Dan seterusnya ...";
 	return 0;
 }
