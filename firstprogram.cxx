f817f4659c753d9b727dfbb647ed2626557282f8
ardiantrik <ardiantrik@gmail.com>
2020-10-04 22:20:12 +0700

    tambah file firstprogram.cxx

---
+++ firstprogram.cxx
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	cout<<"Nama : siapa saya?"<<endl;
+	cout<<"NIM  : 123000000"<<endl;
+	cout<<"Dan seterusnya ...";
+	return 0;
+}
+
